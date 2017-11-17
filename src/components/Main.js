2835b376fdf9274c273bb960038e4c66825a7ede
Rodrigo Passos <rodrigopassos@mac.com>
2017-11-17 17:38:05 -0500

    improve styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,71 @@
+// Modules
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MainStatusBar from './MainStatusBar';
-import styled from 'styled-components/native';
+import { Text, Button } from 'react-native';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-`
+// Components
+import MainStatusBar from './MainStatusBar';
+
+// Styles
+import { Container } from '../utils/styles';
+
+import { connect } from 'react-redux';
+import { fetchDecks, addDeck, clearDecks } from '../actions/decks';
 
 class Main extends Component {
+
+  state = {
+    isReady: false
+  }
+
+  componentDidMount() {
+    this.props.fetchDecks();
+  }
+
+  onPressCreateDeck = () => {
+    
+    this.props.addDeck({
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    });
+  }
+
+  onPressReset = () => {
+    this.props.clearDecks();
+  }
+
   render() {
+    
+    const { decks } = this.props;
     return (
       <Container>
         <MainStatusBar barStyle='light-content' />
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button 
+          onPress={this.onPressCreateDeck}
+          title="Add Deck"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text>{JSON.stringify(decks)}</Text>
+        <Button 
+          onPress={this.onPressReset}
+          title="Clear Decks"
+          accessibilityLabel="Learn more about this purple button"
+        />
       </Container>
     );
   }
 }
 
-export default Main;
+const mapStateToProps  = ({ decks }) => ({
+  decks
+});
+
+export default connect(mapStateToProps, { fetchDecks, addDeck, clearDecks })(Main);
