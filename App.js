984f0918fe4b117d2900ae3d0f8b7a1e82f7e1c3
Rodrigo Passos <rodrigopassos@mac.com>
2017-11-17 15:14:10 -0500

    add redux-thunk

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import Main from './src/components/Main';
 
 import reducers from './src/reducers';
 
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <Main />
       </Provider>
     );
