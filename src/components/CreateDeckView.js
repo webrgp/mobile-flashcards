f0778d4571c89ab172f11752d9adb84011da5e70
Rodrigo Passos <rodrigopassos@mac.com>
2017-11-20 21:37:53 -0500

    connect to store, save and go back to list view

--- src/components/CreateDeckView.js
+++ src/components/CreateDeckView.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, Button, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Picker } from 'react-native';
+import { Text, View, Button, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
-
+import { connect } from 'react-redux';
+import { addDeck } from '../actions/decks';
 import TextField from './TextField';
 
-import { blue, white, lightBlue, lightGray, gray, colors } from '../utils/colors';
+import { getRandomColor, white, lightBlue, lightGray, gray } from '../utils/colors';
 import { NewDeckTitle, BtnText } from '../utils/styles';
 
 class CreateDeckView extends Component {
@@ -25,7 +26,22 @@ class CreateDeckView extends Component {
 
   state = {
     text: '',
-    color: ''
+    showError: false
+  }
+
+  validateDeckName = () => {
+    const { decks } = this.props;
+    if (!(this.state.text in decks)) {
+      const deck = {
+        title: this.state.text.trim(),
+        color: getRandomColor(),
+        questions: []
+      };
+      this.props.addDeck(deck);
+      this.props.navigation.goBack('DeckListView');
+    } else {
+      this.setState({ showError: true });
+    }
   }
 
   saveDeck = () => {
@@ -36,6 +52,11 @@ class CreateDeckView extends Component {
 
     const isEmpty = this.state.text === '';
 
+    // Javascript: {
+    //   title: 'Javascript',
+    //   color: red
+    // }
+
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: white }}>
         <ScrollView style={{ flex: 1 }}>
@@ -45,7 +66,7 @@ class CreateDeckView extends Component {
             onChangeText={(text) => this.setState({text})}
           />
           <TouchableOpacity
-            onPress={ () => { this.saveDeck() }}
+            onPress={ () => { this.validateDeckName() }}
             disabled={isEmpty}
           >
             <BtnText style={ isEmpty ? { backgroundColor: lightGray, color: gray } : {} }>Add New Deck</BtnText>
@@ -56,4 +77,8 @@ class CreateDeckView extends Component {
   }
 }
 
-export default CreateDeckView;
+const mapStateToProps  = ({ decks }) => ({
+  decks
+});
+
+export default connect(mapStateToProps, {addDeck})(CreateDeckView);
